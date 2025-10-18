b9de4208942533537433a0fd5608dc7605d94d5d
Apurba Roy <apurbaroy285@gmail.com>
2025-10-18 13:04:09 +0530

    Add files via upload

---
+++ new.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VotingSystem {
+    struct Proposal {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    address public owner;
+    uint public proposalCount;
+    bool public votingActive;
+
+    mapping(uint => Proposal) public proposals;
+    mapping(address => bool) public hasVoted;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    modifier whenVotingActive() {
+        require(votingActive, "Voting is not active");
+        _;
+    }
+
+    event ProposalCreated(uint id, string name);
+    event Voted(address voter, uint proposalId);
+    event VotingStatusChanged(bool status);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addProposal(string memory _name) public onlyOwner {
+        proposalCount++;
+        proposals[proposalCount] = Proposal(proposalCount, _name, 0);
+        emit ProposalCreated(proposalCount, _name);
+    }
+
+    function startVoting() public onlyOwner {
+        votingActive = true;
+        emit VotingStatusChanged(true);
+    }
+
+    function endVoting() public onlyOwner {
+        votingActive = false;
+        emit VotingStatusChanged(false);
+    }
+
+    function vote(uint _proposalId) public whenVotingActive {
+        require(!hasVoted[msg.sender], "You have already voted");
+        require(_proposalId > 0 && _proposalId <= proposalCount, "Invalid proposal");
+
+        hasVoted[msg.sender] = true;
+        proposals[_proposalId].voteCount++;
+
+        emit Voted(msg.sender, _proposalId);
+    }
+
+    function getProposal(uint _id) public view returns (string memory name, uint voteCount) {
+        Proposal memory proposal = proposals[_id];
+        return (proposal.name, proposal.voteCount);
+    }
+}
